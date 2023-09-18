345898f53c3b17937a32d2df3a910975be6bd0cb
Nadiia <holineinadia@gmail.com>
2023-09-18 15:45:19 +0300

    add start files

--- src/app/[lang]/page.jsx
+++ src/app/[lang]/page.jsx
@@ -6,22 +6,7 @@ export default async function Home({ params }) {
   return (
     <>
       {lang && (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <form>
-            <div>
-              <label>{lang.form.name}</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>{lang.form.email}</label>
-              <input type="email" />
-            </div>
-            <div>
-              <label>{lang.form.city}</label>
-              <input type="text" />
-            </div>
-          </form>
-        </main>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
       )}
     </>
   );
