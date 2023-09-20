bd3d77441de8f8060e80e1fa9f8cafbf836da79e
BogdanGlazkov <bglazkov@i.ua>
2023-09-20 17:28:55 +0300

    small fixes

--- src/app/[lang]/page.jsx
+++ src/app/[lang]/page.jsx
@@ -1,19 +1,8 @@
 import { CurrentFundraising } from '@/sections/CurrentFundraising';
-import { Container } from '@/components/Container';
 import { getDictionary } from '@/utils/getDictionary';
 
 export default async function Home({ params: { lang } }) {
   const staticPageData = await getDictionary(lang);
 
-  return (
-    <>
-      {staticPageData && (
-        <main className="flex min-h-screen flex-col items-center justify-between">
-          <Container>
-            <CurrentFundraising lang={lang} />
-          </Container>
-        </main>
-      )}
-    </>
-  );
+  return <>{staticPageData && <CurrentFundraising lang={lang} />}</>;
 }
