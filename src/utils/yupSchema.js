3fcc95033c4be15dde4622c441de8a9206c55b81
BogdanGlazkov <bglazkov@i.ua>
2023-09-22 12:31:18 +0300

    add error localization

--- src/utils/yupSchema.js
+++ src/utils/yupSchema.js
@@ -1,21 +1,36 @@
 import * as Yup from 'yup';
 
+const url = window?.location?.href;
+const errors = url.includes('uk')
+  ? {
+      required: 'Це поле обовʼязкове',
+      wrongName: 'Неправильне імʼя',
+      wrongPhone: 'Неправильний телефон',
+      wrongMessage: 'Введіть від 3 до 500 символів',
+    }
+  : {
+      required: 'This field is required',
+      wrongName: 'Wrong name',
+      wrongPhone: 'Wrong phone',
+      wrongMessage: 'Type 3 to 500 characters',
+    };
+
 export const formSchema = Yup.object().shape({
   name: Yup.string()
     .trim()
-    .required('This field is required')
-    .min(3, 'Wrong name')
-    .max(70, 'Wrong name')
+    .required(errors.required)
+    .min(3, errors.wrongName)
+    .max(70, errors.wrongName)
     .matches(
       /^(?!.*(\s-|-\s)|.*--|.*——|.*––|.*–\s|.*\s–|.*—\s|.*\s—)[а-яА-Яa-zA-Z\s'’\-–—ЇїІіЄєҐґ]*$/,
-      'Wrong name',
+      errors.wrongName,
     ),
   phone: Yup.string()
-    .required('This field is required')
-    .matches(/^\+\d{11,12}$/, 'Wrong phone'),
+    .required(errors.required)
+    .matches(/^\+\d{11,12}$/, errors.wrongPhone),
   message: Yup.string()
     .trim()
-    .required('This field is required')
-    .min(3, 'Wrong message')
-    .max(500, 'Wrong message'),
+    .required(errors.required)
+    .min(3, errors.wrongMessage)
+    .max(500, errors.wrongMessage),
 });
