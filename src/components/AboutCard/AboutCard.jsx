36a77096d60e2fd0be232e3bc5fbbc1ad64a2861
BogdanGlazkov <bglazkov@i.ua>
2023-09-25 21:28:03 +0300

    add AboutCard component

---
+++ src/components/AboutCard/AboutCard.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+export const AboutCard = ({ data, index, Icon }) => {
+  const { title, paragraph } = data;
+
+  return (
+    <li
+      className={`${
+        index === 3 ? 'xl:h-[393px]' : 'xl:h-[629px]'
+      } w-[279px] h-[486px] md:w-[287px] md:h-[590px] px-5 py-6 border-2 border-tertiary rounded-10`}
+    >
+      <div className="w-[64px] h-[64px] md:w-[88px] md:h-[88px] xl:w-[123px] xl:h-[123px] mx-auto">
+        <Icon />
+      </div>
+      <h3 className="textGradient text-transparent bg-clip-text text-[24px] font-medium leading-normal pt-4 pb-2">
+        {title}
+      </h3>
+      <p>{paragraph}</p>
+    </li>
+  );
+};
+
+AboutCard.propTypes = {
+  data: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    paragraph: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+  icon: PropTypes.func.isRequired,
+};
--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -2,15 +2,30 @@ import PropTypes from 'prop-types';
 
 import { Container } from '@/components/Container';
 import { SectionTitle } from '@/components/SectionTitle';
+import { AboutCard } from '@/components/AboutCard';
+import Aim from 'public/icons/aim.svg';
+import Priorities from 'public/icons/priorities.svg';
+import Team from 'public/icons/team.svg';
+import Achievements from 'public/icons/achievements.svg';
 
 export const About = ({ data }) => {
   const { title, benefits } = data;
-  console.log(benefits);
+  const icons = [Aim, Priorities, Team, Achievements];
 
   return (
     <section className="section" id="about">
       <Container>
         <SectionTitle title={title} />
+        <ul className="flex gap-6">
+          {benefits.map((benefit, index) => (
+            <AboutCard
+              key={index}
+              data={benefit}
+              Icon={icons[index]}
+              index={index}
+            />
+          ))}
+        </ul>
       </Container>
     </section>
   );
