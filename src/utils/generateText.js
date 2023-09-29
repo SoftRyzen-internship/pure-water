2a19c928631df03304c163a83de2408a723a396a
BogdanGlazkov <bglazkov@i.ua>
2023-09-29 22:05:47 +0300

    fix < symbol error

--- src/utils/generateText.js
+++ src/utils/generateText.js
@@ -1,6 +1,7 @@
 export const generateText = data => {
   const string = Object.entries(data)
     .sort((a, b) => a[0].length - b[0].length)
-    .reduce((str, [key, val]) => (str += `${key}: ${val} \n \n`), '');
+    .reduce((str, [key, val]) => (str += `${key}: ${val} \n \n`), '')
+    .replaceAll('<', '&lt;');
   return string;
 };
