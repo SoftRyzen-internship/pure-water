7fe593c9077a4d900302411b4906f600864faf7f
Olenteva74 <olenteva74@ukr.net>
2023-10-01 21:25:31 +0300

    add burgerMenu button

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Contacts } from '@/components/Contacts';
@@ -5,6 +8,7 @@ import { CreatedBy } from '@/components/CreatedBy';
 import { Logo } from '@/components/Logo';
 import { SocialLinks } from '@/components/SocialLinks';
 import { NavBar } from '@/components/NavBar';
+import MenuIcon from 'public/icons/burger-menu.svg';
 
 export const Footer = ({
   contacts,
@@ -16,8 +20,11 @@ export const Footer = ({
   menuLabel,
   navArray,
 }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuToggle = () => setIsMenuOpen(!isMenuOpen);
+
   return (
-    <section className="w-full section pt-[138px] md:pt-[199px] xl:pt-[454px] footer text-white">
+    <footer className="w-full section pt-[138px] md:pt-[199px] xl:pt-[454px] footer text-white">
       <div className="container flex justify-between">
         <div className="hidden xl:block w-[130px]">
           <Logo logoAria={logoAria} lang={lang} />
@@ -30,13 +37,19 @@ export const Footer = ({
           <h3 className="h-8 text-xl font-medium mb-2">{socialsTitle}</h3>
           <SocialLinks section="footer" socials={socials} />
         </div>
-        <div className="w-12 h-12 xl:hidden"></div>
+        <button
+          type="button"
+          className="block xl:hidden w-12 h-12"
+          onClick={menuToggle}
+        >
+          <MenuIcon className="fill-white w-12 h-12" />
+        </button>
         <div className="hidden xl:block">
           <h3 className="h-8 mb-2 text-xl font-medium text-end">{menuLabel}</h3>
           <NavBar section="footer" navArray={navArray} />
         </div>
       </div>
-    </section>
+    </footer>
   );
 };
 
