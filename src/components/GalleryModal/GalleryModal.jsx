93f3ebe2aef70a3ec356309c77c9b7d6e5baf4de
Anna-Voronina <anna.vs2512@gmail.com>
2023-10-10 18:40:06 +0300

    add portal

--- src/components/GalleryModal/GalleryModal.jsx
+++ src/components/GalleryModal/GalleryModal.jsx
@@ -6,6 +6,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 import PropTypes from 'prop-types';
 
+import { ModalPortal } from '../ModalPortal/ModalPortal';
 import { ProjectSwiperNav } from '../ProjectSwiperNav';
 import CloseIcon from 'public/icons/close.svg';
 
@@ -48,107 +49,109 @@ export const GalleryModal = ({ imageList, setIsModalOpen, staticData }) => {
     imageList.length < 6 ? 'md:flex md:justify-center md:items-center' : '';
 
   return (
-    <div
-      onClick={handleBackdropClick}
-      className="fixed z-50 top-0 left-0 flex justify-center xl:items-center w-[100vw] h-screen pt-9 px-5 md:pt-4 md:px-9 xl:py-10 bg-[#020f14bf] backdrop-blur-[25px] overflow-auto"
-    >
-      <div className="relative max-h-[360px] md:max-h-full flex flex-col gap-y-6 md:gap-y-4 max-w-xs md:max-w-3xl xl:max-w-7xl mx-auto">
-        <button
-          onClick={handleCloseBtnClick}
-          type="button"
-          className="xl:absolute xl:top-0 xl:right-[-10%] p-[11px] self-end text-[#ffffffbf] hover:text-white transition-colors duration-300"
-          aria-label={staticData?.closeBtn}
-        >
-          <CloseIcon className="w-[26px] h-[26px] stroke-[currentColor] stroke-[3] stroke-linecap-round" />
-        </button>
+    <ModalPortal>
+      <div
+        onClick={handleBackdropClick}
+        className="fixed z-50 top-0 left-0 flex justify-center xl:items-center w-[100vw] h-screen pt-9 px-5 md:pt-4 md:px-9 xl:py-10 bg-[#020f14bf] backdrop-blur-[25px] overflow-auto"
+      >
+        <div className="relative max-h-[360px] md:max-h-full flex flex-col gap-y-6 md:gap-y-4 max-w-xs md:max-w-3xl xl:max-w-7xl mx-auto">
+          <button
+            onClick={handleCloseBtnClick}
+            type="button"
+            className="xl:absolute xl:top-0 xl:right-[-10%] p-[11px] self-end text-[#ffffffbf] hover:text-white transition-colors duration-300"
+            aria-label={staticData?.closeBtn}
+          >
+            <CloseIcon className="w-[26px] h-[26px] stroke-[currentColor] stroke-[3] stroke-linecap-round" />
+          </button>
 
-        <div className="relative max-w-[280px] md:max-w-[696px] xl:max-w-[842px] md:pb-9 xl:pb-0">
-          <Swiper
-            loop={false}
-            navigation={{ enabled: false }}
-            grabCursor={true}
-            spaceBetween={12}
-            slidesPerView={1}
-            thumbs={{
-              swiper: '.swiper-thumbs',
-              slideThumbActiveClass: 'swiper-slide-thumb-active',
-            }}
-            modules={[FreeMode, Navigation, Thumbs]}
-            className="!mb-6"
-            lazyPreloadPrevNext={1}
-            breakpoints={{
-              768: {
-                spaceBetween: 24,
-              },
-              1280: {
-                navigation: {
-                  enabled: true,
-                  prevEl: '.prevBtn',
-                  nextEl: '.nextBtn',
-                  disabledClass: 'opacity-30',
+          <div className="relative max-w-[280px] md:max-w-[696px] xl:max-w-[842px] md:pb-9 xl:pb-0">
+            <Swiper
+              loop={false}
+              navigation={{ enabled: false }}
+              grabCursor={true}
+              spaceBetween={12}
+              slidesPerView={1}
+              thumbs={{
+                swiper: '.swiper-thumbs',
+                slideThumbActiveClass: 'swiper-slide-thumb-active',
+              }}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="!mb-6"
+              lazyPreloadPrevNext={1}
+              breakpoints={{
+                768: {
+                  spaceBetween: 24,
                 },
-              },
-            }}
-          >
-            {imageList.map(({ id, image, alt }) => (
-              <SwiperSlide
-                key={id}
-                className="!w-[280px] md:!w-[696px] xl:!w-[842px]"
-              >
-                <Image
-                  src={image.secure_url}
-                  alt={alt}
-                  width={842}
-                  height={630}
-                  quality={100}
-                  className="!static !w-[280px] !h-[210px] md:!w-[696px] md:!h-[520px] xl:!w-[842px] xl:!h-[630px] object-contain"
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+                1280: {
+                  navigation: {
+                    enabled: true,
+                    prevEl: '.prevBtn',
+                    nextEl: '.nextBtn',
+                    disabledClass: 'opacity-30',
+                  },
+                },
+              }}
+            >
+              {imageList.map(({ id, image, alt }) => (
+                <SwiperSlide
+                  key={id}
+                  className="!w-[280px] md:!w-[696px] xl:!w-[842px]"
+                >
+                  <Image
+                    src={image.secure_url}
+                    alt={alt}
+                    width={842}
+                    height={630}
+                    quality={100}
+                    className="!static !w-[280px] !h-[210px] md:!w-[696px] md:!h-[520px] xl:!w-[842px] xl:!h-[630px] object-contain"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
 
-          <Swiper
-            ref={thumbsSwiper}
-            watchSlidesProgress={true}
-            slidesPerView={4}
-            freeMode={true}
-            loop={false}
-            centeredSlides={false}
-            spaceBetween={12}
-            wrapperClass={`${mobileWrapperClasses}${tabletWrapperClasses}`}
-            className="swiper-thumbs"
-            modules={[FreeMode, Navigation, Thumbs]}
-            lazyPreloadPrevNext={4}
-            breakpoints={{
-              768: {
-                slidesPerView: 6,
-                lazyPreloadPrevNext: 6,
-              },
-            }}
-          >
-            {imageList.map(({ id, image, alt }) => (
-              <SwiperSlide key={id} className="cursor-pointer opacity-30">
-                <Image
-                  src={image.secure_url}
-                  alt={alt}
-                  width={130}
-                  height={88}
-                  quality={100}
-                  className="!static !w-[61px] h-[48px] md:!w-[106px] xl:!w-[130px] md:h-[80px] xl:h-[88px] object-contain"
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+            <Swiper
+              ref={thumbsSwiper}
+              watchSlidesProgress={true}
+              slidesPerView={4}
+              freeMode={true}
+              loop={false}
+              centeredSlides={false}
+              spaceBetween={12}
+              wrapperClass={`${mobileWrapperClasses}${tabletWrapperClasses}`}
+              className="swiper-thumbs"
+              modules={[FreeMode, Navigation, Thumbs]}
+              lazyPreloadPrevNext={4}
+              breakpoints={{
+                768: {
+                  slidesPerView: 6,
+                  lazyPreloadPrevNext: 6,
+                },
+              }}
+            >
+              {imageList.map(({ id, image, alt }) => (
+                <SwiperSlide key={id} className="cursor-pointer opacity-30">
+                  <Image
+                    src={image.secure_url}
+                    alt={alt}
+                    width={130}
+                    height={88}
+                    quality={100}
+                    className="!static !w-[61px] h-[48px] md:!w-[106px] xl:!w-[130px] md:h-[80px] xl:h-[88px] object-contain"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
+        <ProjectSwiperNav
+          variant="gallery"
+          staticData={{
+            prevBtn: staticData?.prevBtn,
+            nextBtn: staticData?.nextBtn,
+          }}
+        />
       </div>
-      <ProjectSwiperNav
-        variant="gallery"
-        staticData={{
-          prevBtn: staticData?.prevBtn,
-          nextBtn: staticData?.nextBtn,
-        }}
-      />
-    </div>
+    </ModalPortal>
   );
 };
 
